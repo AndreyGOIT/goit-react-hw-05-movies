1b0d47a935eee9729ff7e90dfc1ab288180d03c5
AndreyGOIT <ae.borga@gmail.com>
2022-12-26 14:14:05 +0200

    trending list on main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,35 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 export const Home = () => {
+  // const keyAPI = '1eb36deae800d0e3d9fd1b0466458d26';
+  // const url = 'https://api.themoviedb.org/3/movie/';
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.themoviedb.org/3/trending/movie/day?api_key=1eb36deae800d0e3d9fd1b0466458d26'
+      )
+      .then(function (response) {
+        // console.log(response.data.results);
+        setMovies(response.data.results);
+      });
+  }, []);
+  // console.log(movies);
+
   return (
     <main>
       <h1>Trending today</h1>
-      {/* <img src="https://via.placeholder.com/960x240" alt="" /> */}
-      <p>
-        Здесь будет список фильмов, которые нужно зарендерить сюда как список
-        самых популярных фильмов на сегодня для создания коллекции на главной
-        странице.
-      </p>
+
+      {movies &&
+        movies.map(movie => (
+          <li key={movie.id.toString()}>
+            <Link to={`${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
     </main>
   );
 };
