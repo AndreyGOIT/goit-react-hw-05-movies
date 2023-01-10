b85580bd76ebb0fd095b6b62e014524ec90facaa
AndreyGOIT <ae.borga@gmail.com>
2023-01-10 10:57:56 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,35 @@
 import { Routes, Route } from 'react-router-dom';
-import { Container } from './App.styled';
-import { lazy } from 'react';
+// import { Container } from './App.styled';
+// import { lazy } from 'react';
 
-const Layout = lazy(() => import('./Layout/Layout'));
-const Home = lazy(() => import('../pages/Home/Home'));
-const Movies = lazy(() => import('../pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('../pages/Cast/Cast'));
-const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
+// const Layout = lazy(() => import('./Layout/Layout'));
+// const Home = lazy(() => import('../pages/Home/Home'));
+// const Movies = lazy(() => import('../pages/Movies/Movies'));
+// const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+// const Cast = lazy(() => import('../pages/Cast/Cast'));
+// const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
+
+import Layout from './Layout/Layout';
+import Home from '../pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+import MovieDetails from './MovieDetails/MovieDetails';
+import Cast from '../pages/Cast/Cast';
+import Reviews from '../pages/Reviews/Reviews';
 
 export const App = () => {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:moviesId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
+    // <Container>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:moviesId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
-
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </Container>
+      </Route>
+      <Route path="*" element={<Home />} />
+    </Routes>
+    // </Container>
   );
 };
