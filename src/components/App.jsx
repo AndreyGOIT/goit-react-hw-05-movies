55b35160a0651877c58a7d9231bf64a28493b004
AndreyGOIT <ae.borga@gmail.com>
2023-01-10 11:35:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 // import { Container } from './App.styled';
-import { lazy } from 'react';
+// import { lazy } from 'react';
 import Layout from './Layout/Layout';
+import Home from '../pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+import MovieDetails from './MovieDetails/MovieDetails';
+import Cast from '../pages/Cast/Cast';
+import Reviews from '../pages/Reviews/Reviews';
 
 // const Layout = lazy(() => import('./Layout/Layout'));
-const Home = lazy(() => import('../pages/Home/Home'));
-const Movies = lazy(() => import('../pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('../pages/Cast/Cast'));
-const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
-
-// import Home from '../pages/Home/Home';
-// import Movies from 'pages/Movies/Movies';
-// import MovieDetails from './MovieDetails/MovieDetails';
-// import Cast from '../pages/Cast/Cast';
-// import Reviews from '../pages/Reviews/Reviews';
+// const Home = lazy(() => import('../pages/Home/Home'));
+// const Movies = lazy(() => import('../pages/Movies/Movies'));
+// const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+// const Cast = lazy(() => import('../pages/Cast/Cast'));
+// const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
 
 export const App = () => {
   return (
